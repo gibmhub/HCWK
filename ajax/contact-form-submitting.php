b423210ace301fa1ef772c557da88d4f279060f1
Nils Fischer <n.fischer@viwid.com>
2014-04-12 18:21:30 +0200

    added kontakt email cc

--- ajax/contact-form-submitting.php
+++ ajax/contact-form-submitting.php
@@ -13,7 +13,8 @@
 	$receiver = 'info@hcwk.de';
 	$subject = 'Nachricht von '.$name;
 	$mailheader = "Content-type: text/html; charset=utf-8\r\n";
-	$mailheader .= 'From: '.$email;
+	$mailheader .= 'From: '.$email."\r\n";
+	$mailheader .= 'Cc: ott_jonas@web.de'."\r\n";
 	$message_body = '<p>Name: '.$name.'</p><p>Nachricht:</br>'.$msg.'</p>';
 	mail($receiver, $subject, $message_body, $mailheader) or die('Error sending Email');
 	echo 'success';
