2b3b1973049450818597e7c775b120317c17c653
Nils Fischer <n.fischer@viwid.com>
2014-04-14 20:21:48 +0200

    added faq item

--- data/faq.php
+++ data/faq.php
@@ -5,8 +5,8 @@
 	$faq_items = array();
 
 	$faq = new FAQItem();
-	$faq->frage = 'Wird für das leibliche Wohl gesorgt?';
-	$faq->antwort = 'klaaaro';
+	$faq->frage = 'Kann ich meinen Namen, der auf dem Namensschild erscheint, nach der Registrierung noch ändern?';
+	$faq->antwort = 'Ja, Sie können die Registrierung ihres Ticket-Codes einfach mit den geänderten Daten erneut durchführen. Die bei uns hinterlegten Daten werden dabei aktualisiert.';
 	$faq_items[] = $faq;
 	
 ?>
