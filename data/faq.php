4bd8406fe8d87d52040b06670671382b35327fa2
Nils Fischer <n.fischer@viwid.com>
2014-04-14 20:58:40 +0200

    minor layout refactoring and added faq

--- data/faq.php
+++ data/faq.php
@@ -5,8 +5,23 @@
 	$faq_items = array();
 
 	$faq = new FAQItem();
+	$faq->frage = 'Wozu dient die Registrierung meines Tickets?';
+	$faq->antwort = 'Mit dem Namen, den Sie hier bei der Registrierung angeben, werden wir Ihnen ein Namensschild drucken. Dieses dient Ihnen außerdem während der Veranstaltungszeit als Eintrittskarte.';
+	$faq_items[] = $faq;
+
+	$faq = new FAQItem();
+	$faq->frage = 'Wo kann ich mein Namensschild abholen?';
+	$faq->antwort = 'Zu Beginn des Symposiums erhalten Sie Ihr Namensschild bei der Anmeldung am Uniplatz. Bitte bringen Sie einen Lichtbildausweis mit, der den bei der Registrierung angegebenen Namen trägt.';
+	$faq_items[] = $faq;
+
+	$faq = new FAQItem();
 	$faq->frage = 'Kann ich meinen Namen, der auf dem Namensschild erscheint, nach der Registrierung noch ändern?';
 	$faq->antwort = 'Ja, Sie können die Registrierung ihres Ticket-Codes einfach mit den geänderten Daten erneut durchführen. Die bei uns hinterlegten Daten werden dabei aktualisiert.';
+	$faq_items[] = $faq;
+	
+	$faq = new FAQItem();
+	$faq->frage = 'Muss ich mich auch registrieren, wenn ich ein Online-Ticket gekauft habe?';
+	$faq->antwort = 'Nein, beim Online-Kauf wird ihr Name direkt bei uns hinterlegt. Sie haben diesbezüglich bereits eine Bestätigung per Email von uns erhalten.';
 	$faq_items[] = $faq;
 	
 ?>
--- sidebar.php
+++ sidebar.php
@@ -1,49 +1,53 @@
-<div class="brand-box">
-	<a href="/"><img class="logo" src="/img/brand/logo-light.svg"></a>
-	<div class="date"><i>8. – 10. Mai 2014</i></div>
-</div>
+<div id="sidebar">
 
-<?php
-
-	if (count($current_page->childPages)>0) {
-    	$parentPage = $current_page;
-	} else if (isset($current_page->parentPage)) {
-    	$parentPage = $current_page->parentPage;
-	}
-	if (isset($parentPage)&&count($parentPage->childPages)>0) {
-        echo '<h3>'.$parentPage->title.'</h3>';
-		echo '<ul class="submenu-box">';
-		foreach ($parentPage->childPages as $child) {
-			echo '<li';
-			if ($pagemap->isCurrentPage($child)) {
-    			echo ' class="active"';
-			}
-			echo '><a href="'.$child->displayURL().'">'.$child->title.'</a></li>';
-		}
-		echo '</ul>';
-	}
+	<div class="brand-box">
+		<a href="/"><img class="logo" src="/img/brand/logo-light.svg"></a>
+		<div class="date"><i>8. – 10. Mai 2014</i></div>
+	</div>
 	
-?>
-
-<?php
-	
-//	if ($current_page->id!='news') {
-        include_once('data/news-items.php');
-
-        echo '<h3><a href="/news">Aktuell</a></h3>';
-        echo $news_manager->getNewsBox(5);
-	
-//	}	
-    
-?>
-
-<!--<div id="rnd-referent">
-	<h3>Wir stellen vor:</h3>
 	<?php
-	    include_once('data/referenten.php');
-	    include_once('data/vortraege.php');
-	    $referenten = Referent::allReferenten();
-	    $referent = $referenten[array_rand($referenten)];
-	    echo $referent->getReferentBox('vertical');
+	
+		if (count($current_page->childPages)>0) {
+	    	$parentPage = $current_page;
+		} else if (isset($current_page->parentPage)) {
+	    	$parentPage = $current_page->parentPage;
+		}
+		if (isset($parentPage)&&count($parentPage->childPages)>0) {
+	        echo '<h3>'.$parentPage->title.'</h3>';
+			echo '<ul class="submenu-box">';
+			foreach ($parentPage->childPages as $child) {
+				echo '<li';
+				if ($pagemap->isCurrentPage($child)) {
+	    			echo ' class="active"';
+				}
+				echo '><a href="'.$child->displayURL().'">'.$child->title.'</a></li>';
+			}
+			echo '</ul>';
+		}
+		
 	?>
-</div>-->
+	
+	<?php
+		
+	//	if ($current_page->id!='news') {
+	        include_once('data/news-items.php');
+	
+	        echo '<h3><a href="/news">Aktuell</a></h3>';
+	        echo $news_manager->getNewsBox(5);
+		
+	//	}	
+	    
+	?>
+	
+	<!--<div id="rnd-referent">
+		<h3>Wir stellen vor:</h3>
+		<?php
+		    include_once('data/referenten.php');
+		    include_once('data/vortraege.php');
+		    $referenten = Referent::allReferenten();
+		    $referent = $referenten[array_rand($referenten)];
+		    echo $referent->getReferentBox('vertical');
+		?>
+	</div>-->
+
+</div>
